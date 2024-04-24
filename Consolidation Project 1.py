783868485d99b6dc2d8f1b1162fd9c2e27afee70
Chris Castillo <ccastil4@terpmail.umd.edu>
2024-04-23 20:07:42 -0400

    Organize the structure of the code that will be written

--- Consolidation Project 1.py
+++ Consolidation Project 1.py
@@ -8,4 +8,22 @@
 import random
 import time
 
+# Define a class to represent each player
 
+
+# Define a class to manage the game
+
+
+# Generate a standard deck with 13 cards in each of the four suits
+
+
+# Define a method that shuffles the deck and deals five cards to each player
+
+
+# Define a method that plays a turn of the game
+
+
+# Define a method that starts the game and displays instructions
+
+
+# Set up of the game
